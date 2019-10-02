2e331ba562234a68ee28bea0e68f28a61414dae5
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-10-02 08:34:41 -0600

    Hides credentials with environment variables

--- cypress/integration/login-spec.js
+++ cypress/integration/login-spec.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
 
 describe('Logs in to Namely', () => {
     it('Navigates to Namely page and logs in the user', () => {
         cy.visit('/')
-        cy.get('#user_email').type('matthew.tedder@techtonic.com')
-        cy.get('#user_password').type('')
+        cy.get('#user_email').type(Cypress.env('email'))
+        cy.get('#user_password').type(Cypress.env('password'))
         cy.get('button.btn').click()
     })
 })
